8b44689b81329bd351c9b31f791f4f0ef141f88f
Ethan Chaplin Lee <chaplinleee@gmail.com>
2025-11-25 17:35:25 -0500

    V.1.3

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -30,7 +30,7 @@ export default function Hero() {
         </div>
 
         {/* Right: iPhone (portrait) video */}
-        <div className="flex justify-center ml-12 sm:ml-28 sm:translate-x-16">
+        <div className="flex justify-center ml-12 sm:ml-24 sm:translate-x-16">
           <div className="w-full max-w-sm md:max-w-md lg:max-w-lg lg:mr-2 xl:mr-6 md:mr-12">
             <div
               className="overflow-hidden bg-black/40 shadow-soft border border-white/10"
--- app/components/ReviewsStrip.tsx
+++ app/components/ReviewsStrip.tsx
@@ -3,7 +3,7 @@ import ElfsightGoogleReviews from "./ElfsightGoogleReviews";
 export default function ReviewsStrip() {
   return (
     <section className="bg-white">
-      <div className="section">
+      <div className="section pb-12">
         <div className="mt-6">
           <ElfsightGoogleReviews />
         </div>
